50c3ef7ca235353d0a1edff45fdbd4ba87a6303d
“Joseph <joseph.d.jaafari@gmail.com>
2016-07-22 13:46:53 -0400

    stylized

--- fingerprint.js
+++ fingerprint.js
@@ -2,24 +2,25 @@
  * Joseph Jaafari
  * July 15, 2016
  */
+
 var lookup = function(zip) {
         //borrowed from http://docs.cartodb.com/cartodb-platform/cartodb-js/sql/
         var sql = new cartodb.SQL({
             user: 'jjaafari'
         });
 
-        sql.execute("SELECT county, felon_crimes_no_print_county, total_crimes_not_printed_county, police_department, pd_not_printed, pd_no_print_rate from county_copy where county_id in (SELECT DISTINCT id FROM county where zip = {{zip}}) ORDER BY pd_no_print_rate desc limit 1"
-        ,{zip:zip})
+        sql.execute("SELECT county, felon_crimes_no_print_county, total_crimes_not_printed_county, police_department, pd_not_printed, pd_no_print_rate from county_copy where county_id in (SELECT DISTINCT id FROM county where zip = {{zip}}) ORDER BY pd_not_printed desc limit 1", {
+            zip: zip
+        })
             .done(function(data) {
                 var results = data.rows[0]
 
                 console.log(results);
 
-                $("#felons_no_print").html("In " + results.county + ", there are a total of " + results.total_crimes_not_printed_county + " charges that have no fingerprints attached to them. Out of those, " +
+                $("#felons_no_print").html("In " + results.county + " County, there are a total of " + results.total_crimes_not_printed_county + " charges that have no fingerprints attached to them. Out of those, " +
                       results.felon_crimes_no_print_county + " were felonies."); 
                 
-                $("#biggestOffender").html("The " + results.police_department + " had the lowest fingerprinting rate at " + results.pd_no_print_rate + ". The office did not print " +  results.pd_not_printed + 
-                    " people who were charged with crimes in 2015."); 
+                $("#biggestOffender").html("The " + results.police_department + " did not fingerprint " + results.pd_not_printed + " people for criminal charges, the highest in the county. The office's average fingerprint rate for all crimes in 2015 was " + results.pd_no_print_rate + "."); 
             })
             
             .error(function(errors) {
@@ -32,5 +33,7 @@ $(document).ready(function() {
 
     $("body").on("click", "#calc", function() {
         var zip = parseFloat($("#zipCode").val());
-        lookup(zip);})
+        lookup(zip);
     });
+
+  });
